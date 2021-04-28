b67c3ff83b436942935bbe6d40642f814bfa4f23
Jaibean <jaimie.bertoli@gmail.com>
2021-04-27 22:45:10 -0400

    updates to projects section and structure

--- updated_personal_portfolio/js/parallax.js
+++ updated_personal_portfolio/js/parallax.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', function () {
     parallax.style.transform = 'translateY(' + scrollPosition * .5 + 'px)';
 });
 
-// onclick 
+// onclick unhide contents
 function scrollFunction() {
     x = document.getElementById("Content").style; 
     if (x.display === "none" || x.display == '') {
@@ -17,6 +17,33 @@ function scrollFunction() {
     }
   }
   
+// onclick scroll
+
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+
+
+
 
   $(document).ready(function() {
     if($(".splash").is(":visible")) {
